49affb705e2feeaa5177f65d178d6cc3d8566ae6
sach7x <sachin2592@gmail.com>
2018-08-06 23:00:47 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,55 +1,25 @@
-import datetime as dt
-import numpy as np
-import pandas as pd
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
-import sqlite3
-
-from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-
-#################################################
-# Database Setup
-#################################################
-# engine = create_engine("mysql://root:@localhost/Project_Three") <-- tried with mySQL file, did not work so switched to sqlite file
-engine = create_engine("sqlite:///NIH_data.sqlite")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-NIH_Data = Base.classes.nih_data
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
-#flask setup
+from flask import Flask, render_template
 app = Flask(__name__)
-db = SQLAlchemy(app) 
 
-#querying PI names to test connection, looks like this is working
 @app.route("/")
-def database():
-    results = session.query(NIH_Data.pi).all()
+def dashboard():
+    return render_template('index.html') #"Render Dashboard!"
 
-    all_names = list(np.ravel(results))
+@app.route("/")
+def presentation():
+    return render_template('presentation.html') #"Render Presentation page!"
 
-    return jsonify(all_names)
+@app.route("/")
+def tables():
+    return render_template('tables.html') #"Render Tables page!"
 
-#testing to see if table is displayed to web. not successful
-#get error with db. apparently db not defined but I defined it earlier in the code..?
-@app.route("/NIH_Data")
-def NIH_Table():
-    cursor = db.execute('SELECT column1,column2,column3,column4 FROM NIH_data')
-    return render_template('NIH_Data_Web.html', items=cursor.fetchall())
+@app.route("/")
+def FEDRePORTER_table():
+    return render_template('FEDReporter_table.html') #Render  NIH Federal RePORTER Tables page
 
+@app.route("/")
+def SunburstChart():
+    return render_template('sunburst.html') # Render sunburst chart    
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
